56249bd93858dac6c5de01446dafcb59489b2f81
Łukasz Kostyk <lukas.kostyk@gmail.com>
2026-07-18 21:18:00 +0200

    Fix social pfps not showing

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'ddragon.leagueoflegends.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
     ],
   },
 };
